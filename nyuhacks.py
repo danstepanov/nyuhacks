0af164e350ba4a5febabfcc9d6a01f8c7ec0c5f1
Jon H.M. Chan <me@jonhmchan.com>
2013-11-16 20:08:25 -0500

    Nimbus setup

--- nyuhacks.py
+++ nyuhacks.py
@@ -1,18 +1,58 @@
+import tornado
 import tornado.ioloop
 import tornado.web
+from tornado.options import define, options
+
+import logging
+
+import datetime
+
+import os.path
 #import libraries
 
-# *** USER SUBMISSION ***
-class Title():
-    title = raw_input("Title: ")
-		if len(input_str) > 30:
-    		print "Only 30 characters allowed."
-    sys.exit()
+# Define Port
+define("port", default=8888, help="run on the given port", type=int)
 
-class Story():
-	story = raw_input("Story: ")
-		if len(input_str) > 500:
-    		print "Only 500 characters allowed."
-    sys.exit()
+class Application(tornado.web.Application):
 
-class Location():
+    def __init__(self, debug = False):
+        handlers = [
+            # Home page
+            (r"/", MainHandler)
+        ]
+        settings = dict(
+            cookie_secret="/Vo=",
+            login_url="",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            facebook_api_key="",
+            facebook_secret="",
+            debug=debug
+        )
+
+        # logging.info("Static URL: {}".format(settings['static_path']))
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+# Main Runtime
+def main():
+    tornado.options.parse_command_line()
+    logging.info("starting webserver on 0.0.0.0:%d" % tornado.options.options.port)
+    print("Web server started again at "+str(datetime.datetime.now()))
+    app = Application(debug = (True if options.port==8888 else False))
+    app.listen(options.port)
+
+    # Create IOLoop
+    mainloop = tornado.ioloop.IOLoop
+
+    # Create scheduled cron jobs
+    interval_ms = 1000
+
+    # Start main loop
+    mainloop.instance().start()
+
+
+if __name__ == "__main__": main()
